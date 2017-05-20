6cfc5ff3a27c8da6a6670197b4dec6b61b8a0cc2
Josu <info@josu-astigarraga.es>
2017-05-20 19:45:35 +0200

    Avances en ngc-grid

--- js/ngcApp.js
+++ js/ngcApp.js
@@ -29,11 +29,31 @@ angular.module("ngcApp",["ngRoute","ngAnimate"])
 	}];
 })
 .controller("ngcClientesController",function($scope){
+	function data(d){
+		if(typeof d.date !== "undefined"){
+			d.date = d.date.toLocaleString();
+		}
+		return d;
+	}
 	$scope.table = {
 		transport:{
 			read:{
 				url: apiurl("Clientes"),
 				method:"GET"
+			},
+			insert:{
+				url:apiurl("ClientesInsert"),
+				data:data
+			},
+			update:{
+				url:apiurl("ClientesUpdate"),
+				data:function(n,old){
+					return {new:data(n),old:data(old)};
+				}
+			},
+			delete:{
+				url:apiurl("ClientesDelete"),
+				data:data
 			}
 		},
 		columns:[
@@ -63,17 +83,29 @@ angular.module("ngcApp",["ngRoute","ngAnimate"])
 				hedaer:"Correo",
 				colWidth:"200px",
 				templateUrl:tediturl("BaseCellTemplate.html"),
-				editTemplateUrl:tediturl("BaseCellEditTemplate.html")
+				editTemplateUrl:tediturl("BaseEmailTemplate.html")
 			},
 			{
 				name:"date",
 				header:"Fecha",
 				colWidth:"140px",
-				templateUrl:tediturl("BaseCellTemplate.html"),
-				editTemplateUrl:tediturl("BaseCellEditTemplate.html")
+				templateUrl:tediturl("BaseDateTemplate.html"),
+				editTemplateUrl:tediturl("BaseDateEditTemplate.html")
 			}
-		]
-	}
+		],
+		pageSize:"5",
+		page:1,
+		pageSizes: [{
+			value:"5",
+		},{
+			value:"10",
+		},{
+			value:"20",
+		},{
+			value:"",
+			text:"Todos"
+		}]
+	};
 })
 .directive("ngcGrid",["$http",function($http){
 	return{
@@ -86,29 +118,150 @@ angular.module("ngcApp",["ngRoute","ngAnimate"])
 		templateUrl:turl("Grid.html"),
 		link: function(s,e,a,c){
 			s.editIndex = -1;
+			s.spinnerText = "Cargando..."
 			s.setEditIndex = function(index){
 				s.inserting = false;
 				s.editIndex = index;
 				s.view = angular.copy(s.table.rows[index]);
-			}
+			};
+			s.showInsertRow = function(){
+				s.editIndex = -1;
+				s.view = {};
+				s.inserting = true;
+			};
 			s.cancel = function(){
 				s.view = {};
 				s.inserting = 0;
 				s.editIndex = -1;
-			}
-			s.read = function(){
-				var r = ((s.table.transport || {}).read || {});
+			}; 
+			s.delete = function(index){
+				s.cancel();
+				if(confirm("¿Seguro que desea eliminar el registro?")){
+					s.loading = true;
+					var d = ((s.table.transport || {}).delete || {});
+					if(d.url){
+						$http({
+							url:d.url,
+							method:d.method||"POST",
+							data:jQuery.param(s.table.rows[index]),
+							headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+						}).then(function(response){
+							s.table.rows.splice(index,1);
+							s.loading = false;
+						},function(error){
+							alert(JSON.stringify(error));
+							s.loading = false;
+						})
+					}else{
+						s.table.rows.splice(index,1);
+						s.loading = false;
+					}
+				}
+				
+			};
+			s.insert = function(){
+				var r = ((s.table.transport || {}).insert || {});
+				s.loading = true;
 				if(r.url){
 					$http({
 						url:r.url,
-						method:(r.method || "GET"),
-					}).then(function(resp){
-						s.table.rows = resp.data;
-					},function(error){
-
-					});
+						method:r.method||"POST",
+						data:jQuery.param(typeof r.data ==="undefined"?s.view:r.data(s.view)),
+						headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+					}).then(function(response){
+						if(typeof r.success === "undefined"){
+							if(typeof response.data !== "undefined"){
+								if(typeof response.data.length !== "undefined"){
+									for(var i in response.data){
+										s.table.rows.unshift(response.data[i])
+									}
+								}else{
+										s.table.rows.unshift(response);
+								}
+							}else{
+									s.table.unshift(response)
+							}
+						}else{
+							r.success(response);		
+						}
+						s.view = {};
+						s.inserting = false;
+						s.loading = false;
+				},function(error){
+						alert(JSON.stringify(error));
+					
+				});
+				}else{
+					s.table.rows.unshift(angular.copy(s.view));
+					s.view = {};
+					s.inserting = false;
+					s.loading = false;
 				}
 			};
+			s.update = function(index){
+				s.loading = true;
+				var u  = ((s.table.transport || {}).update || {});
+				if(u.url){
+					$http({
+						url:u.url,
+						method:u.method ||"POST",
+						data:jQuery.param(typeof u.data ==="undefined"?{new:s.view,old:s.table.rows[index]}:u.data(s.view,s.table.rows[index])),
+						headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+					}).then(function(response){
+						var data = response.data || reponse;
+						data = data[0] || data;
+						s.table.rows[index] = data;
+						s.editIndex = -1;
+						s.loading = false;
+					},function(error){
+						alert(JSON.stringify(error));
+						s.loading = false;
+					})
+				}else {
+					s.table.rows[index] = angular.copy(s.view);
+					s.editIndex = -1;
+					s.loading = false;
+				}
+			}
+			s.read = function(){
+				s.inserting = false;
+				s.editIndex = -1;
+				s.view = {};
+				var r = ((s.table.transport || {}).read || {});
+				var data = {
+					page:s.table.page || 1,
+					pageSize:s.table.pageSize
+				};
+				if(r.url){
+					s.loading = true;
+					$http({
+						url:r.url,
+						method:(r.method || "GET"),
+						params:(typeof r.data ==="undefined"?data:r.data(data)),
+						headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+					}).then(function(resp){
+						s.table.total = resp.data.total;
+						s.table.from = ((s.table.page || 1) - 1) * s.table.pageSize + 1;
+						s.table.rows = resp.data.data || resp.data || r;
+						s.table.to = s.table.from + s.table.rows.length - 1;
+						s.pageCount = Math.ceil(s.table.total/s.table.pageSize);
+						s.pages = [];
+						var from = (s.table.page || 1) - 3;
+						from = from<1?1:from;
+						for(from;from<=s.pageCount && ((s.table.page||1)+3)>=from;from++){
+							s.pages.push(from);
+						} 
+						s.loading = false;
+					},function(error){
+						s.loading=false;
+					});
+				}
+
+			};
+			s.setPage = function(page){
+				s.table.page = page;
+				s.read();
+			}
 			s.ngcGrid = {
 				read: s.read
 			};
--- ngc-cumples.api.php
+++ ngc-cumples.api.php
@@ -3,9 +3,147 @@ global $wpdb;
 $n =  'ngc-api/v1';
 register_rest_route($n,"/Clientes",[
 		"methods"=>"GET",
-		"callback"=>function(){
+		"callback"=>function($req){
 			global $wpdb;
-			echo json_encode($wpdb->get_results("SELECT * FROM ngc_customer"));
+			$result = [];			$params = $req->get_params();
+			$sql = "SELECT * FROM ngc_customer";
+			if(isset($params["pageSize"]) && $params["pageSize"] != "-1"){
+				$sql .= " LIMIT " .($params["pageSize"] * ($params["page"] - 1).",".$params["pageSize"]);
+			}
+			$result["total"] = $wpdb->get_var("SELECT COUNT(*) FROM ngc_customer");
+			$result["data"] = $wpdb->get_results($sql);
+			die(json_encode($result));
+		}
+	]);
+function sanitize_and_validate($input,$ruleset){
+	$result = [];
+	foreach ($ruleset as $name => $rules) {
+		if(isset($rules["sanitize"])){
+			$result[$name] = esc_sql($rules["sanitize"]((isset($input[$name])?$input[$name]:null)));
+		}else{
+			$result[$name] = esc_sql((isset($input[$name])?$input[$name]:null));
+		}
+		if(isset($rules["validate"]) && !$rules["validate"]($result[$name])){
+			$error_message = $rules["validate"]($result[$name]);
+			if($error_message !== null){
+				throw new Exception($error_message);
+			}
+		}
+	}
+	return $result;
+}
+function ngc_r($n,$lmin,$lmax,$email=false){
+	return [
+			"sanitize"=>function($e){
+					return htmlspecialchars($e,ENT_QUOTES);
+				},
+			"validate"=>function($e) use($n,$lmin,$lmax,$email){
+				if(strlen($e)>$lmax){
+					return "El campo $n no puede tener mas de $lmax carácteres";
+				}
+				if(strlen($e)<$lmin){
+					return "El campo $n no puede tener menos de $lmin carácteres";
+				}
+				if($email && !	filter_var($e, FILTER_VALIDATE_EMAIL)){
+					return "El campo $n no es una dirección de correo válida";
+				}
+			}
+		];
+}
+function get_customer($input){
+	$customer = sanitize_and_validate($input,[
+			"idCustomer"=>[
+				"sanitize"=>function($e=null){
+					if(isset($e) && $e !== null){
+						return (int)$e;
+					}
+					return 0;
+				},
+				"validate"=>function($e){
+					if($e !=(int)$e){
+						return "El campo id debe de ser un número";
+					}
+				}
+			],
+			"name"=>ngc_r("nombre",2,40),
+			"surname1"=>ngc_r("primer apellido",2,40),
+			"surname2"=>ngc_r("segundo apellido",2,40),
+			"email"=>ngc_r("email",2,40,true),
+			"date"=>[
+				"sanitize"=>function($e){
+					return filter_var($e,FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				},
+				"validate"=>function($e){
+					if($e != date($e)){
+						return "El campo fecha ha de ser una fecha válida";
+					}
+				}
+			]
+		]);
+	return $customer;
+}
+register_rest_route($n,"/ClientesUpdate",[
+		"methods"=>"POST",
+		"callback"=>function($req){
+			global $wpdb;
+			try {
+				$data = $req->get_params();
+
+				$ctr = get_customer($data["new"]);
+				$ctrOld = get_customer($data["old"]);
+				$wpdb->query("UPDATE ngc_customer SET name='".$ctr["name"]."', surname1='".$ctr["surname1"]."',
+					surname2='".$ctr["surname2"]."',email='".$ctr["email"]."',date='".$ctr["date"]."' WHERE idCustomer=".$ctr["idCustomer"]);
+				die(json_encode($wpdb->get_results("SELECT * FROM ngc_customer WHERE idCustomer=".$ctr["idCustomer"])));
+			}
+			catch (Exception $ex){
+				http_response_code(500);
+				die(json_encode($ex));
+			}
 			die();
+		}
+	]);
+register_rest_route($n,"/ClientesInsert",[
+		"methods"=>"POST",
+		"callback"=>function($req){
+			global $wpdb;
+			try{
+				$ctr = get_customer($req->get_params());
+				$wpdb->query("BEGIN");
+				$wpdb->query("INSERT INTO ngc_customer (name,surname1,surname2,email,date) VALUES ('".$ctr["name"]."',
+					'".$ctr["surname1"]."','".$ctr["surname2"]."','".$ctr["email"]."','".$ctr["date"]."')");
+				echo json_encode($wpdb->get_results("SELECT * FROM ngc_customer WHERE idCustomer = LAST_INSERT_ID()"));
+				if($wpdb->last_error !== ''){
+    				$wpdb->print_error();
+    				$wpdb->query("ROLLBACK");
+				}
+				$wpdb->query("COMMIT");
+    			die();
+			}catch(Exception $ex){
+				http_response_code(500);
+				die(json_encode($ex));
+			}
+			die();
+		}
+	]);
+register_rest_route($n,"/ClientesDelete",[
+		"methods"=>"POST",
+		"callback"=>function($req){
+			global $wpdb;
+			try{
+			$ctr = get_customer($req->get_params());
+			if(isset($ctr["idCustomer"])){
+				$id = $ctr["idCustomer"];
+				$wpdb->query("DELETE FROM ngc_customer WHERE idCustomer=$id");
+				if(isset($wpdb->last_error) && $wpdb->last_error){
+					http_response_code(500);
+					die("No se pudo eliminar: " . $wpdb->last_error);
+				}
+				http_response_code(200);
+				die("{\"msg\":\"Ok\"}");
+			}
+			}catch(Exception $ex){
+				http_response_code(500);
+				die($ex->get_message());
+			}
 		}
 	]);
